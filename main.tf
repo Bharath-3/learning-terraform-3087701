6a1d01acfe282c38aa396a59616539cb1f9d27c7
Bharath-3 <85668103+Bharath-3@users.noreply.github.com>
2025-12-16 20:51:29 +0000

    load balancer

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws.instance.blog.id
+      target_id        = aws_instance.blog.id
     }
   }
 
